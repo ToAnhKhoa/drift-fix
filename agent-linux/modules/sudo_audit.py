ecf3ec6744e0036d34ef1eb92a4a69866b59ace2
Khoa <ititiu21225@student.hcmiu.edu.vn>
2026-01-10 10:56:39 -0800

    testing linux refactor

--- agent-linux/modules/sudo_audit.py
+++ agent-linux/modules/sudo_audit.py
@@ -1,3 +1,34 @@
-def check_and_remediate_admins(allowed_admins):
-    # Tạm thời trả về False (chưa xử lý) để code chạy được
+import grp
+import subprocess
+
+def check_and_remediate_admins(allowed_list):
+    
+    ADMIN_GROUP = "wheel" 
+    drift_detected = False
+    details = []
+
+    try:
+     
+        current_admins = grp.getgrnam(ADMIN_GROUP).gr_mem
+    except KeyError:
+        return True, f"Critical: Group '{ADMIN_GROUP}' not found!"
+
+    
+    unauthorized_users = [user for user in current_admins if user not in allowed_list]
+
+    if unauthorized_users:
+        drift_detected = True
+        
+        for user in unauthorized_users:
+            print(f"   [SECURITY FIX] Removing UNAUTHORIZED ADMIN: {user}...")
+            
+           
+            cmd = f"gpasswd -d {user} {ADMIN_GROUP}"
+            subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            
+            details.append(f"Removed '{user}'")
+
+    if drift_detected:
+        return True, f"Admin Group Cleaned: {', '.join(details)}"
+    
     return False, None
